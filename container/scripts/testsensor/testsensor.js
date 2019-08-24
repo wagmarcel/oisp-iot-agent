c8a37e09a4c7cc6ab903490673f12c3cead2671b
Marcel Wagner <wagmarcel@web.de>
2019-08-24 14:57:52 +0000

    updated testsensor with logger and using mounted /etc/oisp/sensorSpecs.json if existing

--- container/scripts/testsensor/testsensor.js
+++ container/scripts/testsensor/testsensor.js
@@ -4,74 +4,72 @@
 // TEST_SAMPLES: if set number of samples to send before terminating
 // COMPONENT_TYPE: name of the component type
 // COMPONENT_NAME: name of the component
+// LOG_LEVEL: log level
 "use strict";
 var dgram = require('dgram');
-var PD = require("probability-distributions");
+var PD = require('probability-distributions');
+var winston = require('winston');
+var fs = require('fs');
+const specsFileName = '/etc/oisp/sensorSpecs.json';
 
-var PORT = 41234;
-var HOST = '127.0.0.1';
-var values = {};
-var numSamples = 0;
-var componentType = "temperature.v1.0";
-var componentName = "temp";
-var testSamples;
-var sensorSpecs= [
+var logLevel = process.env.LOG_LEVEL;
+if (logLevel === undefined) {
+    logLevel = 'warn';
+}
+const logger = winston.createLogger({
+    level: logLevel,
+    transports: [
+	new winston.transports.Console()
+    ]
+})
+
+var default_port = 41234;
+var default_host = '127.0.0.1';
+var default_sensorSpecs = [
     {
 	agents: [
 	    {
-		port: 41234,
-		host: "127.0.0.1"
-	    },
-	    {
-		port: 41244,
-		host: "127.0.0.1"
+		port: default_port,
+		host: default_host
 	    }
 	],
 	name: "tempSensor",
 	componentName: "temp",
 	componentType: "temperature.v1.0",
 	type: "number",
-	sigma: 0.1,
+	sigma: 0.3,
 	startValue: 20
-    },
-    {
-	agents: [
-	    {
-		port: 41244,
-		host: "127.0.0.1"
-	    }
-	],
-	name: "stateSensor",
-	componentName: "internalState",
-	componentType: "machineDescription.v1.0",
-	type: "string",
-	startValue: "mydeviceinfo"
     }
 ]
+
+var values = {};
+var numSamples = 0;
+var testSamples;
+
+var sensorSpecs = [];
+
+if (fs.existsSync(specsFileName)) {
+    sensorSpecs = require(specsFileName);
+    logger.info("SensorSpec found in /etc/oisp. Loaded: " + JSON.stringify(sensorSpecs));
+}
+else {
+    sensorSpecs = default_sensorSpecs;
+    logger.info("SensorSpec NOT found in /etc/oisp. Default: " + JSON.stringify(sensorSpecs));
+}
 
 if (process.env.TEST_SAMPLES) {
     testSamples = process.env.TEST_SAMPLES;
 }
-
-/*if (process.env.COMPONENT_TYPE) {
-    componentType = process.env.COMPONENT_TYPE;
-}
-if (process.env.COMPONENT_NAME) {
-    componentName = process.env.COMPONENT_NAME;
-}*/
-
-
 
 var registerComponent = function(spec) {
     var component = { "t": spec.componentType, "n": spec.componentName }
     var comp_message = new Buffer(JSON.stringify(component));
     spec.agents.forEach(function(agent){
 	var client = dgram.createSocket('udp4');
-	console.log("Marcel registring: ", agent);
+	logger.verbose("Registring component: " + JSON.stringify(component) + " at url " + agent.host + ":" + agent.port);
 	client.send(comp_message, 0, comp_message.length, agent.port, agent.host, function(err, response) {
-            if (err) console.log("Error:", err);
+            if (err) logger.error("Error: " + err);
             client.close();
-            console.log("registering " + JSON.stringify(component));
 	});
     });
 }
@@ -109,10 +107,10 @@ sensorSpecs.forEach(function(spec){
 	    //to be on the save side re-register. Agent will realize if already existing
 	    registerComponent(spec);
 	    var value = values[spec.name];
-	    var telemetry = { "n": componentName, "v": value };
+	    var telemetry = { "n": spec.componentName, "v": value };
 	    sendObservation(spec, telemetry, function(err, bytes) {
-		if (err) console.log("Error:", err);
-		console.log(telemetry)
+		if (err) logger.error("Error:" + err);
+		logger.info(telemetry)
 		if (spec.type === "number") {
 		    var change = PD.rnorm(1, 0, spec.sigma);
 		    value += change[0];
@@ -120,7 +118,7 @@ sensorSpecs.forEach(function(spec){
 		}
 		numSamples++;
 		if (testSamples && numSamples >= testSamples) {
-		    console.log("Maximal number of testsamples reached. Terminating!")
+		    logger.info("Maximal number of testsamples reached. Terminating!")
 		    process.exit(0);
 		}
 	    })
